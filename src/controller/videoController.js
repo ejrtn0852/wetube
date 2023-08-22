0676052a924b3513793a4f301f06989a7ea1343a
ejrtn0852 <135151859+ejrtn0852@users.noreply.github.com>
2023-08-22 18:49:05 +0900

    5,8

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,4 +1,8 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "home"});
+
+export const trending = (req, res) => {
+    const videos = [1,2,3,4,5,6,7,8,9,10];
+    return res.render("home", {pageTitle: "home", videos});
+}
 export const see = (req, res) =>  res.render("watch", {pageTitle: "watch"});
 export const edit = (req, res) => res.render("edit", {pageTitle: "edit"});
 export const search = (req, res) => res.send("search!");
