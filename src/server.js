0526ad78e086b24da4ed21cd51ea7fa61f9e0e4a
ejrtn0852 <ejrtn0852@naver.com>
2023-09-12 23:08:03 +0900

    github login end

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ import './models/Video';
 import express, { Router } from 'express';
 import morgan from 'morgan';
 import session from "express-session";
+import MongoStore from 'connect-mongo';
 import rootRouter from './routers/rootRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 import { localsMiddleware } from './middlewares.js/middlewares';
+
 
 const app = express();
 const logger = morgan('dev');
@@ -16,10 +18,14 @@ app.set('views', process.cwd() + '/src/views');
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
-    secret: "Hello",
-    resave:true,
-    saveUninitialized: true,
-}))
+    secret: process.env.COOKIE_SECRET,
+    resave:false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+    cookie: {
+        maxAge:10000,
+    }
+}));
 app.use(localsMiddleware);
 app.use('/', rootRouter);
 app.use('/video', videoRouter);
