e9949b11ba2a6b08bb11bf88765feeb1ae4e312d
ejrtn0852 <ejrtn0852@naver.com>
2023-09-18 01:30:21 +0900

    volume end

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,45 @@
-console.log("video Hi");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volumeRange = document.getElementById("volume");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handlePlayClick = (e) => {
+    video.paused ? video.play() : video.pause();
+};
+
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {
+    if (video.muted) {
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    volumeRange.value = video.muted ? 0 : volumeValue;
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+};
+
+const handleVolumeChange = (event) => {
+    const {
+        target: { value },
+    } = event;
+    if (video.muted) {
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    } else {
+        video.muted = true;
+    }
+    volumeValue = value;
+    video.volume = volumeValue;
+};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
+volumeRange.addEventListener("input", handleVolumeChange);
