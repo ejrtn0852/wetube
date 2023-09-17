c707582b021464dac5a4d09807ecda01d94809e0
ejrtn0852 <ejrtn0852@naver.com>
2023-09-17 23:40:21 +0900

    front start

--- src/server.js
+++ src/server.js
@@ -34,6 +34,7 @@ app.use(
 );
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/video", videoRouter);
 app.use("/users", userRouter);
