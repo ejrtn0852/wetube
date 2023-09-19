9e58e06d927a70dd75d0b5051d0f91fc9062307b
ejrtn0852 <ejrtn0852@naver.com>
2023-09-19 23:47:23 +0900

    wowowow

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,10 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -17,11 +19,11 @@ const handlePlay = () => (playBtn.innerText = "Pause");
 const handleMute = (e) => {
     if (video.muted) {
         video.muted = false;
+        volumeRange.value = 0;
     } else {
         video.muted = true;
+        volumeRange.value = volumeValue;
     }
-    volumeRange.value = video.muted ? 0 : volumeValue;
-    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
 };
 
 const handleVolumeChange = (event) => {
@@ -33,9 +35,28 @@ const handleVolumeChange = (event) => {
         muteBtn.innerText = "Mute";
     } else {
         video.muted = true;
+        muteBtn.innerText = "stop";
     }
     volumeValue = value;
     video.volume = volumeValue;
+};
+
+const formatTime = (seconds) =>
+    new Date(seconds * 1000).toISOString().substring(14, 19);
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = video.duration;
+};
+
+const handleTimeUpdate = () => {
+    currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (event) => {
+    const { value } = event.target;
+    video.currentTime = value;
 };
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -43,3 +64,6 @@ muteBtn.addEventListener("click", handleMute);
 video.addEventListener("pause", handlePause);
 video.addEventListener("play", handlePlay);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
